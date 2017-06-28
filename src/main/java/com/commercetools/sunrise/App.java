5f88101b6c4ca72cf27bbdf6d88cd6154bb62c07
Laura Luiz <laura.luiz@commercetools.de>
2017-06-28 13:33:22 +0200

    Use project with blog data

--- src/main/java/com/commercetools/sunrise/App.java
+++ src/main/java/com/commercetools/sunrise/App.java
@@ -22,25 +22,25 @@ public class App {
 
         final String spaceId = properties.getProperty("spaceId");
         final String accessToken = properties.getProperty("accessToken");
-        final String productContentTypeId = "2PqfXUJwE8qSYKuM0U6w8M";
+        final String productContentTypeId = "2wKn6yEnZewu2SCCkus4as";
         final String pageSlugFieldId = "slug";
 
         final CmsService cmsService = ContentfulCmsService.of(spaceId, accessToken, productContentTypeId, pageSlugFieldId, ForkJoinPool.commonPool());
 
-        printProduct(cmsService, "hudson-wall-cup");
-        printProduct(cmsService, "soso-wall-clock");
-        printProduct(cmsService, "playsam-streamliner-classic-car-espresso");
-
+        printPost(cmsService, "seven-tips-from-ernest-hemingway-on-how-to-write-fiction");
+        printPost(cmsService, "down-the-rabbit-hole");
     }
 
-    private static void printProduct(final CmsService cmsService, final String pageKey) throws ExecutionException, InterruptedException {
+    private static void printPost(final CmsService cmsService, final String pageKey) throws ExecutionException, InterruptedException {
         final CmsPage cmsPage = fetchCmsPage(cmsService, pageKey);
-        System.out.println("*** PRODUCT " + cmsPage.fieldOrEmpty("productName"));
-        System.out.println("Price: " + cmsPage.fieldOrEmpty("price"));
-        System.out.println("Category: " + cmsPage.fieldOrEmpty("categories[0].title"));
-        System.out.println("Brand logo: " + cmsPage.fieldOrEmpty("brand.logo"));
-        System.out.println("Brand founded: " + cmsPage.fieldOrEmpty("brand.foundedYear"));
-        System.out.println("Brand location: " + cmsPage.fieldOrEmpty("brand.location"));
+        System.out.println("*** POST ***");
+        System.out.println("Title: " + cmsPage.fieldOrEmpty("title"));
+        System.out.println("Date: " + cmsPage.fieldOrEmpty("date"));
+        System.out.println("Comments: " + cmsPage.fieldOrEmpty("comments"));
+        System.out.println("Image: " + cmsPage.fieldOrEmpty("featuredImage"));
+        System.out.println("Categories: " + cmsPage.fieldOrEmpty("category[0].title") + " " + cmsPage.fieldOrEmpty("category[1].title"));
+        System.out.println("Author: " + cmsPage.fieldOrEmpty("author[0].name"));
+        System.out.println("Author's entries: " + cmsPage.fieldOrEmpty("author[0].createdEntries[0].title"));
     }
 
     private static CmsPage fetchCmsPage(final CmsService cmsService, final String pageKey) throws InterruptedException, ExecutionException {
